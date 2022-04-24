86e76a2a22beaf303910dec31dd5245729e7491d
gabrielhenrique99 <gabrielhenrique16041999@gmail.com>
2022-04-24 17:58:51 -0300

    first commit

---
+++ src/br/com/teste/Teste.java
@@ -0,0 +1,18 @@
+package br.com.teste;
+
+import br.com.teste.model.Gato;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+		
+		//int a = 2;
+		//int b = 3;
+		//System.out.println("Soma: "+(a+b));
+	}
+
+}
